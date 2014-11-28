ba9fb844a14ec633de6bcf48736a899749d8d7f3
Ameliorate <ameliorate2@gmail.com>
2014-11-27 22:11:07 -0500

    Starting work on a player
    Cerrently have a mobile class that has the ability to move, but going
    any further would require events, and I can't seem to learn those for
    the life of me.

---
+++ Buildstation_Client2/Buildstation_Client2/Class/Mobile.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Buildstation_Client2.Class
+{
+    class Mobile : PhysicalObject
+    {
+        protected void MoveUp() // Moves the player 1 tile up every time it's called.
+        {
+            YPos++;
+        }
+
+        protected void MoveDown() // 1 tle down.
+        {
+            YPos--;
+        }
+
+        protected void MoveLeft() // 1 tile left.
+        {
+            XPos--;
+        }
+
+        protected void MoveRight() // 1 tile right.
+        {
+            YPos++;
+        }
+
+
+
+
+        
+
+        protected void MoveUp(int Distance) // Any ammount up.
+        {
+            YPos += Distance;
+        }
+
+        protected void MoveDown(int Distance) // Any ammount down.
+        {
+            YPos -= Distance;
+        }
+
+        protected void MoveLeft(int Distance) // Any ammount left.
+        {
+            XPos -= Distance;
+        }
+
+        protected void MoveRight(int Distance) // Any ammount right.
+        {
+            XPos += Distance;
+        }
+
+
+    }
+}
