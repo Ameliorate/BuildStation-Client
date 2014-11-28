b1ae5b2891572022631d9bed7e3d6e1abbb3a368
Ameliorate <ameliorate2@gmail.com>
2014-11-27 19:11:33 -0500

    Progress So Far
    Cerrent work: About a half finished basic object class.
    TODO: Finish basic object class, fix error regarding the map array (Why
    doesn't it work?), make the game do something.

---
+++ Buildstation_Client2/Buildstation_Client2/Class/PhysicalObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Buildstation_Client2.Class
+{
+    class PhysicalObject
+    {
+        /*
+         * This is anything that exists, and contains information about possitioning, movement, and anything else that would exist in the world.
+         */
+
+
+        // Where the object is and what are its properties.
+        protected bool IsPassable; // If you can walk through or on it. For example a floor can be walked on, but you couldnt walk on a wall.
+        protected bool IsTransparent; // Can you see through or over it? You can see through a window or a PDA on ther floor, but fou cant see through a wall.
+        protected bool IsDragable; // Can you drag it around or push it?
+        protected string[] OtherProperties; // Any other properties of the object. Such as a table would be flat, waist height, and hard. This makes certan actions be able to depend on what an object is like instead of whitelisting certan objects.
+        protected int XPos; // The X possition on a grid. 0 starts at the top left courner. This is normally set automatically.
+        protected int YPos; // The Y possition on a grid.
+        protected int ZPos;
+        protected string ObjectName; // The name of the object. An example would be SpaceX, FloorAB, or WallHFDHYRFDGTRHTG.
+
+
+        // What the object looks like.
+        protected Texture2D Sprite; // The cerrent apearance of the thing.
+        protected int SpriteSizeX = 48; // How large your sprite is on the X plane. Normally 48 pixels.
+        protected int SpriteSizeY = 48; // How large your sprite is on the Y plane. Normally 48 pixels.
+
+        
+
+        // Actually starting logic.
+        //Map[XPos, YPos, ZPos] = ObjectName; // Is supposed to set the location of the object to the apropiate place on the map. But it seems to hate me.
+
+
+
+        
+
+
+
+
+
+    }
+}
---
+++ Buildstation_Client2/Buildstation_Client2/Class/Variables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Buildstation_Client2.Class
+{
+    static class Variables
+    {
+        // This class contains public varibles ment to be used in many places.
+
+        public static string[, ,] Map = new string[15, 15, 64]; // The map. The contents of the string indacate what object, and is used to refer to a dictionary below witch contains data about the object. Formated [x] [y] [z] where z is how high in the stack or pile of objects the thing is. Preferably having 64 objects on one tile sould have the tile act as if it had an inpassible object on it. After all, it would be a very large pile of stuff.
+        // In theory you can have the same object in different places at the same time, but any change you make to one object will ablo hapen to the otheer. Could be used to simulate quantum psyics though. "This is a quantum entangled paper. Anything you write on it will also be written on an other quantum entagled paper somewhere else."
+
+        public Dictionary<string, string> ObjectData = new Dictionary<string, string>(); // The dictionary that was refered to, be careful to not let things conflict.
+    }
+}
