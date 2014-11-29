d3dd750c4069408f24dc3e62f72fde1195f2a351
Ameliorate <ameliorate2@gmail.com>
2014-11-28 22:55:49 -0500

    Basic Object Name Generation
    Doesn't quite work how I origonally intended, but it's taking up way too
    much time, and at this point, I just want to move on to other things.

---
+++ Buildstation_Client2/Buildstation_Client2/Class/NameTools.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Buildstation_Client2.Class
+{
+    class NameTools
+    {
+        private string ObjectType; 
+        private string[] NameStorage; // Contains the finished names. Probably can be used for something. I think a part in how the rendering will work, but I keep forgeting. Too much information to hold.
+        int[] FirstUnusedChar; // Contains the numerical value of the first unused Charactor. Each spot in the array is another charactor used in combonation.
+         // The names of objects are stored like WallA, FloorBC, or SpaceDSAGFGHDT.
+        private string CerrentName;
+        private int CerrentNameNumber;  //How many names have been generated basically.
+        Random Random = new Random();
+        public NameTools(string ObjectType)
+        {
+            ObjectType = this.ObjectType;
+        }
+
+
+
+        public string GetAtPosition(int Position)   // Return the value of the array at a given spot.
+        {
+            return NameStorage[Position];
+        }
+
+        public int GetLength()
+        {
+            return NameStorage.Length;
+        }
+
+        public string GenerateName(string ObjectType)
+        {
+            CerrentName = ObjectType + Random.Next(10000000); // This method doesnt have any methiods of avoiding name collision, but to much work for such a small chance.
+            NameStorage[CerrentNameNumber] = CerrentName;
+            CerrentNameNumber++;
+            return CerrentName; // Yes, I know that doesnt actually do what I explained earlier, but at this point, I've been working on this for quite a while and I cant find a good solution.
+        }
+    }
+}
--- Buildstation_Client2/Buildstation_Client2/Class/Variables.cs
+++ Buildstation_Client2/Buildstation_Client2/Class/Variables.cs
@@ -11,5 +11,8 @@ namespace Buildstation_Client2.Class
 
         public static string[, ,] Map = new string[15, 15, 64]; // The map. Mostly is used by other objects to see what is under them and to interface with those objects. X and Y are self explanatory, Z is the height of the objects in a pile of objects. 0 is normally the floor.
         // If there are 64 objects on the floor, preferably the tile should be impassible.
+
+        static public char[] AllPossibleCharactors = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+        // Every single possible reasonable charactor on your keyboard. Could include lowercases for almost twice as many, but thats getting unreasonable. Also too much efort to type.
     }
 }
