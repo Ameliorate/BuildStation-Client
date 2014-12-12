6ed189c7b43ab2a2c33e5dbcf1456bc5c9c811a3
Ameliorate <ameliorate2@gmail.com>
2014-12-11 20:40:55 -0500

    Clean up the space object
    It is now much shorter and cleaner by removing 16 if statements with 1
    thing that adds the iconnumber and space.

--- Buildstation_Client2/Buildstation_Client2/Class/Objects/Space.cs
+++ Buildstation_Client2/Buildstation_Client2/Class/Objects/Space.cs
@@ -55,102 +55,13 @@ namespace Buildstation_Client2.Class.Objects
         /// </summary>
         public void Initalise()
         {
-
-
             DoesMove = false;
             Random Random = new Random();
             IconNumber = Random.Next(NumberOfIcons);  // Choses what icon is used for this tile.
-            SetSpriteRandom();                      // Sets the sprite to that icon.
+            SpriteState = "Space" + IconNumber.ToString();                      // Sets the sprite to that icon.
             RotationInDegrees = Random.Next(3) * 90; // Creates 4 varations of each sprite by rotating them, reducing common patterns.
             
             InitaliseTurf();  // Does things relating to turfs. Like makes you able to walk on them and see through them.
-        }
-
-        /// <summary>
-        /// Sets the space tile to a new, random sprite.
-        /// </summary>
-        private void SetSpriteRandom()
-        {
-            // WARNING: Possibly ugly code ahead. Don't say I didn't warn you.
-            if (IconNumber == 0)                                 
-            {
-                SpriteState = "Space0";
-            }
-
-            if (IconNumber == 1)
-            {
-                SpriteState = "Space1";
-            }
-
-            if (IconNumber == 2)
-            {
-                SpriteState = "Space2";
-            }
-
-            if (IconNumber == 3)
-            {
-                SpriteState = "Space3";
-            }
-
-            if (IconNumber == 4)
-            {
-                SpriteState = "Space4";
-            }
-
-            if (IconNumber == 5)
-            {
-                SpriteState = "Space5";
-            }
-
-            if (IconNumber == 6)
-            {
-                SpriteState = "Space6";
-            }
-
-            if (IconNumber == 7)
-            {
-                SpriteState = "Space7";
-            }
-
-            if (IconNumber == 8)
-            {
-                SpriteState = "Space8";
-            }
-
-            if (IconNumber == 9)
-            {
-                SpriteState = "Space9";
-            }
-
-            if (IconNumber == 10)
-            {
-                SpriteState = "Space10";
-            }
-
-            if (IconNumber == 11)
-            {
-                SpriteState = "Space11";
-            }
-
-            if (IconNumber == 12)
-            {
-                SpriteState = "Space12";
-            }
-
-            if (IconNumber == 13)
-            {
-                SpriteState = "Space13";
-            }
-
-            if (IconNumber == 14)
-            {
-                SpriteState = "Space14";
-            }
-
-            if (IconNumber == 15)
-            {
-                SpriteState = "Space15";
-            }
         }
     }
 }
